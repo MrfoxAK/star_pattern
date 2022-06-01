a4d9232c59f582e39a1091a39a849d662a708569
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-06-01 19:14:01 +0530

    Add files via upload

---
+++ half_hole_D_star_p.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the no.of rows: \n");
+    scanf("%d",&n);
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<2*n-1; j++) {
+            if(j<=n-i-1 || j>=n+i-1) {
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
